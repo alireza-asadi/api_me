1c47a815021e6f0ff5a714e121e69bb6a167b007
a.r.a <a.r.asadi82@gmail.com>
2016-04-20 17:58:24 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -6,8 +6,8 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "186655141:AAGR2I8A5h879WQ-oun_tHoK7nESzyue4ek" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
-local You = 121916460  --ID ADMIN ایدی خود را اینجا قرار دهید
++local bot_api_key = "186655141:AAGR2I8A5h879WQ-oun_tHoK7nESzyue4ek" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
++local You = 121916460  --ID ADMIN ایدی خود را اینجا قرار دهید
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
@@ -33,7 +33,7 @@ local help = [[
 `/id` ✴️
 ایدی
 ➖➖➖➖➖➖➖➖➖➖➖
-M.KH @cruel0098
+parsaasadi ( @ara_parsa )
 ]]--
 -------
 
